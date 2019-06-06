b17ec874bda3039f111cefa4df9567ba9edb2cae
capybarracuda <31936603+capybarracuda@users.noreply.github.com>
2019-06-06 12:24:29 -0700

    lab6

--- table.c
+++ table.c
@@ -1,146 +1,288 @@
-/********************************************************
-*		Silas Kidd Myers			*
-* 		COEN12					*
-* 		23 April 2019				*
-*Lab 3: Make a table using a hash function		*
-********************************************************/
+/*
+ * File:        table.c
+ *
+ * Copyright:	2017, Darren C. Atkinson
+ *
+ * Description: This file contains the public and private function and type
+ *              definitions for a set abstract data type for generic
+ *              pointer types.  A set is an unordered collection of unique
+ *              elements.
+ *
+ *              This implementation uses a hash table to store the
+ *              elements, with linear probing to resolve collisions.
+ *              Insertion, deletion, and membership checks are all average
+ *              case constant time.
+ */
 
-#include "set.h"
-#include <string.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <assert.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <assert.h>
+# include <stdbool.h>
+# include "set.h"
+
+# define EMPTY   0
+# define FILLED  1
+# define DELETED 2
+
+struct set {
+    int count;                  /* number of elements in array */
+    int length;                 /* length of allocated array   */
+    void **data;                /* array of allocated elements */
+    char *flags;                /* state of each slot in array */
+    int (*compare)();		/* comparison function         */
+    unsigned (*hash)();		/* hash function               */
+};
 
 /*
-if	sp->flags=0: elt is empty
-	sp->flags=1: elt is filled
-	sp->flags=2: elt is deleted
-*/
+ * Function:    search
+ *
+ * Complexity:  O(1) average case, O(n) worst case
+ *
+ * Description: Return the location of ELT in the set pointed to by SP.  If
+ *		the element is present, then *FOUND is true.  If not
+ *		present, then *FOUND is false.  The element is first hashed
+ *		to its correct location.  Linear probing is used to examine
+ *		subsequent locations.
+ */
 
-typedef struct set//initializes variables that store info about set
+static int search(SET *sp, void *elt, bool *found)
 {
-	char **data;//array of elts
-	int length;//max length of array
-	int count;//keeps track of size
-	int *flags;
-}SET;
-
-SET *sp;
+    int available, i, locn, start;
 
 
-unsigned strhash(char *elt)//hash function-O(n) where n is size of string
-{
-	unsigned hash = 0;
-	while (*elt != '\0')
-		hash = 31 * hash + *elt++;
-	return hash;
+    available = -1;
+    start = (*sp->hash)(elt) % sp->length;
+
+    for (i = 0; i < sp->length; i ++) {
+        locn = (start + i) % sp->length;
+
+        if (sp->flags[locn] == EMPTY) {
+            *found = false;
+            return available != -1 ? available : locn;
+
+        } else if (sp->flags[locn] == DELETED) {
+            if (available == -1)
+		available = locn;
+
+        } else if ((*sp->compare)(sp->data[locn], elt) == 0) {
+            *found = true;
+            return locn;
+        }
+    }
+
+    *found = false;
+    return available;
 }
 
-static int search(char *elt,SET *sp,int *found)//O(n), ideal O(1) searches for an element (passed by elt) in a set(passed by sp), returns the index and whether or not the elt was found(passed by found)
+
+/*
+ * Function:    createSet
+ *
+ * Complexity:  O(m)
+ *
+ * Description: Return a pointer to a new set with a maximum capacity of
+ *		MAXELTS.
+ */
+
+SET *createSet(int maxElts, int (*compare)(), unsigned (*hash)())
 {
-	int i=strhash(elt)%sp->length,clearedElt=-1,foundDeleted=0;
-	while(sp->flags[i]!=0)
-	{
-		if(sp->flags[i]==1)
-			if(strcmp(elt,sp->data[i])==0)
-				return i;
-		if((sp->flags[i]==2)&&(foundDeleted==0))
+    int i;
+    SET *sp;
+
+
+    assert(compare != NULL && hash != NULL);
+
+    sp = malloc(sizeof(SET));
+    assert(sp != NULL);
+
+    sp->data = malloc(sizeof(char *) * maxElts);
+    assert(sp->data != NULL);
+
+    sp->flags = malloc(sizeof(char) * maxElts);
+    assert(sp->flags != NULL);
+
+    sp->compare = compare;
+    sp->hash = hash;
+    sp->length = maxElts;
+    sp->count = 0;
+
+    for (i = 0; i < maxElts; i ++)
+        sp->flags[i] = EMPTY;
+
+    return sp;
+}
+
+
+/*
+ * Function:    destroySet
+ *
+ * Complexity:  O(m)
+ *
+ * Description: Deallocate memory associated with the set pointed to by SP.
+ *		The elements themselves are not deallocated since we did not
+ *		allocate them in the first place.  That's the rule: if you
+ *		didn't allocate it, then you don't deallocate it.
+ */
+
+void destroySet(SET *sp)
+{
+    assert(sp != NULL);
+
+    free(sp->flags);
+    free(sp->data);
+    free(sp);
+}
+
+
+/*
+ * Function:    numElements
+ *
+ * Complexity:  O(1)
+ *
+ * Description: Return the number of elements in the set pointed to by SP.
+ */
+
+int numElements(SET *sp)
+{
+    assert(sp != NULL);
+    return sp->count;
+}
+
+
+/*
+ * Function:    addElement
+ *
+ * Complexity:  O(1) average case, O(n) worst case
+ *
+ * Description: Add ELT to the set pointed to by SP.
+ */
+
+void addElement(SET *sp, void *elt)
+{
+    int locn;
+    bool found;
+
+
+    assert(sp != NULL && elt != NULL);
+    locn = search(sp, elt, &found);
+
+    if (!found) {
+	assert(sp->count < sp->length);
+
+	sp->data[locn] = elt;
+	sp->flags[locn] = FILLED;
+	sp->count ++;
+    }
+}
+
+
+/*
+ * Function:    removeElement
+ *
+ * Complexity:  O(1) average case, O(n) worst case
+ *
+ * Description: Remove ELT from the set pointed to by SP.  A element is
+ *		deleted by changing the state of its slot.
+ */
+
+void removeElement(SET *sp, void *elt)
+{
+    int locn;
+    bool found;
+
+
+    assert(sp != NULL && elt != NULL);
+    locn = search(sp, elt, &found);
+
+    if (found) {
+	sp->flags[locn] = DELETED;
+	sp->count --;
+    }
+}
+
+
+/*
+ * Function:    findElement
+ *
+ * Complexity:  O(1) average case, O(n) worst case
+ *
+ * Description: If ELT is present in the set pointed to by SP then return
+ *		it, otherwise return NULL.
+ */
+
+void *findElement(SET *sp, void *elt)
+{
+    int locn;
+    bool found;
+
+
+    assert(sp != NULL && elt != NULL);
+
+    locn = search(sp, elt, &found);
+    return found ? sp->data[locn] : NULL;
+}
+
+
+int partition(void **a, int lo, int hi, int(*cmp)())//O(n) partitions pivot so that entire left half is less and right half is more
+{
+	void* piv=a[hi];
+	void* temp;
+	int j=lo-1,i;
+	
+	for(i=lo;i<hi;i++)
+		if(cmp(a[i],piv)<=0)
 		{
-			clearedElt=i;
-			foundDeleted=1;
+			j++;
+			temp=a[i];
+			a[i]=a[j];
+			a[j]=temp;
 		}
-		i=(i+1)%sp->length;
+	temp=a[j+1];
+	a[j+1]=a[hi];
+	a[hi]=temp;
+	return j+1;
+}
+
+void quicksort(void **a, int lo, int hi, int(*cmp)())//O(nlogn) sorts partitions recursively
+{
+	int pi;
+	if(lo<hi)
+	{
+		pi=partition(a,lo,hi,cmp);
+		
+		quicksort(a,lo,pi-1,cmp);
+		quicksort(a,pi+1,hi,cmp);
 	}
-	*found=0;
-	if(foundDeleted==1)
-		return clearedElt;
-	return i;	
 }
 
-SET *createSet(int maxElts)//O(m) initializes set with max elements in set passed in, returns set pointer
-{
-	int i;
-	sp=malloc(sizeof(SET));
-	assert(sp!=NULL);
-	sp->length=maxElts;
-	sp->data=malloc(sizeof(char*)*(sp->length));
-	assert(sp->data!=NULL);
-	sp->flags=malloc(sizeof(int*)*(sp->length));
-	assert(sp->flags!=NULL);
-	for(i=0;i<sp->length;i++)
-		sp->flags[i]=0;
-	sp->count=0;
-	return sp;
-}
+/*
+ *Function: *getElements(SET *sp)
+ *
+ *Complexity:O(nlogn)
+ *
+ *Description: returns sorted array of elts
+ *
+ */
 
-void destroySet(SET *sp)//O(m) clears set(passed by sp)
-{
-	int i;
-	assert(sp!=NULL);
-	for(i=0;i<sp->length;i++)
-	{
-		if(sp->flags[i]==1)
-			free(sp->data[i]);
-	}
-	free(sp->flags);
-	free(sp->data);
-	free(sp);
-	return;
-}
-
-int numElements(SET *sp)//O(1) clears set(passed by sp)
+void *getElements(SET *sp)
 {
 	assert(sp!=NULL);
-	return sp->count;	
-}
-
-void addElement(SET *sp, char *elt)//O(n) (ideal O(1))adds elt(passed by elt) to set (pointd by sp)
-{
-	int found=1;
-	int i=search(elt,sp,&found);
-	if((sp->count!=sp->length)&&(found==0))
-	{
-		while(sp->flags[i]==1)
-			i++;
-		sp->flags[i]=1;
-		sp->data[i]=strdup(elt);
-		sp->count++;
-	}	
-}
-
-void removeElement(SET *sp, char *elt)//O(n) expected: O(1) removes elt (passed by elt) from set pointer(passed by sp)
-{
-	int found=1;
-	int index=search(elt,sp,&found);
-	if(found==1)
-	{
-		free(sp->data[index]);
-		sp->flags[index]=2;
-		sp->count--;
-	}	
-}
-
-char *findElement(SET *sp, char *elt)//O(n) expected: O(1) returns the element(passed by elt) in set (passed sp)
-{	
-	int found=1;
-	int i=search(elt,sp,&found);
-	if(found==1)
-		return sp->data[i];
-	return NULL;
-}
-
-char **getElements(SET *sp)//O(m) copies a set(sp) and returns it
-{
-	char **array;
+	void **a;
 	int i,j=0;
-	int size=(sizeof(char*)*((sp->count)));
-	array=malloc(size);
-	assert(array!=NULL);
+	a=malloc(sizeof(void*)*sp->length);
+	assert(a!=NULL);
 	for(i=0;i<sp->length;i++)
 		if(sp->flags[i]==1)
 		{
-			array[j]=strdup(sp->data[i]);
+			a[j]=sp->data[i];
 			j++;
 		}
-	return array;		
+	quicksort(a,0,j-1,sp->compare);
+	return a;
 }
+
+
+
+
+
